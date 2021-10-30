eded0b305ec9fae8b2266ae3834375395472d882
Jacob Hyde <jacob@jacobhyde.com>
2021-10-30 11:40:03 -0400

    fix subscriptions

--- src/App/Http/Requests/SubscriptionPlanRequest.php
+++ src/App/Http/Requests/SubscriptionPlanRequest.php
@@ -13,7 +13,7 @@ class SubscriptionPlanRequest extends FormRequest
      */
     public function authorize()
     {
-        return auth()->check();
+        return auth('api')->check();
     }
 
     /**
